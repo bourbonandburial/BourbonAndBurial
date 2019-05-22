df5341b030481a7b6388d55120dd8d4845143b1c
Maggie Leavell <mjleavell@gmail.com>
2019-05-21 20:10:47 -0500

    getting all customers

---
+++ BourbonAndBurial/Controllers/CustomersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BourbonAndBurial.Data;
+using BourbonAndBurial.Validators;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BourbonAndBurial.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        readonly CustomerRepository _customerRepository;
+
+        public CustomersController()
+        {
+            _customerRepository =  new CustomerRepository();
+        }
+
+        [HttpGet]
+        public ActionResult GetAllCustomers()
+        {
+            var customers = _customerRepository.GetAll();
+
+            return Ok(customers);
+        }
+    }
+}
---
+++ BourbonAndBurial/Data/CustomerRepository.cs
@@ -0,0 +1,27 @@
+﻿using BourbonAndBurial.Models;
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BourbonAndBurial.Data
+{
+    public class CustomerRepository
+    {
+        const string ConnectionString = "Server = localhost; Database = BourbonAndBurial; Trusted_Connection = True;";
+
+        public IEnumerable<Customer> GetAll()
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var getQuery = "SELECT customerId, firstname, lastName, companyName, username, firebaseId FROM customer";
+
+                var customers = db.Query<Customer>(getQuery).ToList();
+
+                return customers;
+            }
+        }
+    }
+}
