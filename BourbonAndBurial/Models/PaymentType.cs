d092b2da2039309859bba9b8ab471b56ff2ea254
Maggie Leavell <mjleavell@gmail.com>
2019-05-23 23:22:59 -0500

    added paymentType class

---
+++ BourbonAndBurial/Models/PaymentType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BourbonAndBurial.Models
+{
+    public class PaymentType
+    {
+        public int PaymentTypeId { get; set; }
+        public string PaymentName { get; set; }
+        public int AcctNumber { get; set; }
+        public int CustomerId { get; set; }
+    }
+}
