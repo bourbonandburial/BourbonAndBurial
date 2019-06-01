ee0d3585f0a7da81055d3002c06c76644d12996a
unknown <c.chase.hamby@gmail.com>
2019-06-01 12:49:08 -0500

    updated startup and api keys

--- BourbonAndBurial/ClientApp/src/helpers/data/constants.js
+++ BourbonAndBurial/ClientApp/src/helpers/data/constants.js
@@ -1,12 +1,12 @@
 const apiKeys = {
     firebaseConfig: {
-        apiKey: "AIzaSyDWFfmkBV3orGJy4pnttO40adSfMBwMvKE",
-        authDomain: "bourbon-and-burial.firebaseapp.com",
-        databaseURL: "https://bourbon-and-burial.firebaseio.com",
-        projectId: "bourbon-and-burial",
-        storageBucket: "bourbon-and-burial.appspot.com",
-        messagingSenderId: "904126841330",
-        appId: "1:904126841330:web:3d1b5ad422128fe1"
+        apiKey: "AIzaSyCOL2xbThH7YknjW4Wln4ONlORlimdqoNs",
+        authDomain: "bourbonandburial-f4ac0.firebaseapp.com",
+        databaseURL: "https://bourbonandburial-f4ac0.firebaseio.com",
+        projectId: "bourbonandburial-f4ac0",
+        storageBucket: "bourbonandburial-f4ac0.appspot.com",
+        messagingSenderId: "873143103044",
+        appId: "1:873143103044:web:b1e8666569b04cd4"
     }
   };
   
