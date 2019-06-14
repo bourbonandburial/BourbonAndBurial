07dee92753868f93a9649edb401efa4a0962fff8
Maggie Leavell <mjleavell@gmail.com>
2019-06-13 21:14:24 -0500

    added register modal files

---
+++ BourbonAndBurial/ClientApp/src/components/RegisterModal/RegisterModal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+class RegisterModal extends React.Component {
+  render() {
+    return (
+      <div className="RegisterModal">
+      </div>
+    );
+  }
+}
+
+export default RegisterModal;
